07d23d2d1f224f483f06a1fbabed00e6b31b5cbd
Eric <54091924+EricHsia7@users.noreply.github.com>
2023-09-02 13:37:47 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-import { Timeweaver, getDurationAndFormat } from './core/index';
-import { searchItemsbyname } from './core/storage';
+import { Timeweaver, getDurationAndFormat } from './core/index.ts';
+import { searchItemsbyname } from './core/storage.ts';
 import { durationToPixel, generateActivityHTML, updateStatus } from './user-interface';
+
+import './user-interface/css/index.'
 
 var weaver;
 
 function weave() {
+    //initialize
   var t = searchItemsbyname('time_weaver_x_').sort(function (a, b) {
     var a = JSON.parse(String(localStorage.getItem(a))).start;
     var b = JSON.parse(String(localStorage.getItem(b))).start;
@@ -12,7 +15,6 @@ function weave() {
   });
   weaver = new Timeweaver(t.length > 0 ? JSON.parse(String(localStorage.getItem(t[0]))) : new Date());
 
-  //initialize
 }
 
 export default weave;
